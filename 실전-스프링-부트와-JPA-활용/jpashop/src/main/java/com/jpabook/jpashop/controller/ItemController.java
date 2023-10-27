fcb248ae15ac2754f2e50a52835a295b80e51dd5
7unho <rlawnsgh8395@naver.com>
2023-10-27 19:13:45 +0900

    :sparkles: Feat: 상품 목록 화면, 로직 작성

--- 실전-스프링-부트와-JPA-활용/jpashop/src/main/java/com/jpabook/jpashop/controller/ItemController.java
+++ 실전-스프링-부트와-JPA-활용/jpashop/src/main/java/com/jpabook/jpashop/controller/ItemController.java
@@ -1,6 +1,7 @@
 package com.jpabook.jpashop.controller;
 
 import com.jpabook.jpashop.domain.item.Book;
+import com.jpabook.jpashop.domain.item.Item;
 import com.jpabook.jpashop.service.ItemService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -8,17 +9,29 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
 @Controller
 @RequiredArgsConstructor
 public class ItemController {
     private final ItemService itemService;
 
+    /**
+     * 상품 등록 폼 요청
+     * @param model
+     * @return
+     */
     @GetMapping("/items/new")
     public String createForm(Model model) {
         model.addAttribute("form", new BookForm());
         return "items/createItemForm";
     }
 
+    /**
+     * 상품 등록 로직
+     * @param bookForm
+     * @return
+     */
     @PostMapping("/items/new")
     public String create(BookForm bookForm) {
         Book book = new Book();
@@ -31,5 +44,18 @@ public class ItemController {
 
         itemService.saveItem(book);
         return "redirect:/";
+    }
+
+    /**
+     * 상품 목록 조회
+     * @param model
+     * @return
+     */
+    @GetMapping("/items")
+    public String list(Model model) {
+        List<Item> items = itemService.findItems();
+        model.addAttribute("items", items);
+
+        return "items/itemList";
     }
 }
