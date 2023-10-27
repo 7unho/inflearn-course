6d17472365263bed25ccf27f487ff2a1352fbb32
7unho <rlawnsgh8395@naver.com>
2023-10-27 18:43:14 +0900

    :safety_vest: Feat: 회원 목록 조회 작성

--- 실전-스프링-부트와-JPA-활용/jpashop/src/main/java/com/jpabook/jpashop/controller/MemberController.java
+++ 실전-스프링-부트와-JPA-활용/jpashop/src/main/java/com/jpabook/jpashop/controller/MemberController.java
@@ -11,18 +11,30 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
 public class MemberController {
     private final MemberService memberService;
 
+    /**
+     * 회원가입 폼 호출
+     * @param model
+     * @return
+     */
     @GetMapping("/members/new")
     public String createForm(Model model) {
         model.addAttribute("memberForm", new MemberForm());
         return "members/createMemberForm";
     }
 
+    /**
+     * 회원가입 로직
+     * @param memberForm
+     * @param result
+     * @return
+     */
     @PostMapping("/members/new")
     public String create(@Valid MemberForm memberForm, BindingResult result) { // @Valid 어노테이션을 사용하면, memberForm의 데이터를 validation 해줌.
         /**
@@ -40,5 +52,13 @@ public class MemberController {
 
         memberService.join(member);
         return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+
+        return "members/memberList";
     }
 }
