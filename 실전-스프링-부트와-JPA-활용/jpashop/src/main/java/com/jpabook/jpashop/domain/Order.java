318ba83e57c6a0e21ae9c57e03e8f5f6da1e0a6c
7unho <rlawnsgh8395@naver.com>
2024-01-14 17:14:08 +0900

    :bulb: Style: 주석 오타 수정

--- 실전-스프링-부트와-JPA-활용/jpashop/src/main/java/com/jpabook/jpashop/domain/Order.java
+++ 실전-스프링-부트와-JPA-활용/jpashop/src/main/java/com/jpabook/jpashop/domain/Order.java
@@ -33,7 +33,7 @@ public class Order {
     private LocalDateTime orderDate;
 
     @Enumerated(EnumType.STRING)
-    private OrderStatus status; // 주문상태 [ ORDER, CALCEL ]
+    private OrderStatus status; // 주문상태 [ ORDER, CANCEL ]
 
     // 연관관계 메서드
     public void setMember(Member member){
