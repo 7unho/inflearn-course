a33a571e3dc8c89bd383153f836a40324d60f206
7unho <rlawnsgh8395@naver.com>
2024-01-14 17:43:21 +0900

    :sparkles: Feat: 회원 등록 API v1 작성

---
+++ 실전-스프링-부트와-JPA-활용/jpashop/src/main/java/com/jpabook/jpashop/api/MemberApiController.java
@@ -0,0 +1,33 @@
+package com.jpabook.jpashop.api;
+
+import com.jpabook.jpashop.domain.Member;
+import com.jpabook.jpashop.service.MemberService;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController // @Controller + @ResponsBody
+@RequiredArgsConstructor
+public class MemberApiController {
+    private final MemberService memberService;
+
+    @PostMapping("/api/v1/members")
+    public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member) { // @RequestBody := Json 데이터를 Member로 매핑해줌
+        Long id = memberService.join(member);
+        return new CreateMemberResponse(id);
+    }
+
+    @Data
+    static class CreateMemberResponse {
+        private Long id;
+
+        public CreateMemberResponse(Long id) {
+            this.id = id;
+        }
+    }
+}
