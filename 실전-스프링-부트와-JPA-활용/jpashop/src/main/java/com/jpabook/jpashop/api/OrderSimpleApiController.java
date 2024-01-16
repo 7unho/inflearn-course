d37406535734a22dd6be17fe14c1e280214b383a
7unho <rlawnsgh8395@naver.com>
2024-01-17 05:30:59 +0900

    :sparkles: Feat: `/api/v1/simple-orders` API 예제 작성

---
+++ 실전-스프링-부트와-JPA-활용/jpashop/src/main/java/com/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -0,0 +1,28 @@
+package com.jpabook.jpashop.api;
+
+import com.jpabook.jpashop.domain.Order;
+import com.jpabook.jpashop.repository.OrderRepository;
+import com.jpabook.jpashop.repository.OrderSearch;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * XtoOne 관계의 성능 최적화
+ * Order
+ * Order -> Member
+ * Order -> Delievery
+ */
+@RestController
+@RequiredArgsConstructor
+public class OrderSimpleApiController {
+    private final OrderRepository orderRepository;
+
+    @GetMapping("/api/v1/simple-orders")
+    public List<Order> orderV1(){
+        List<Order> all = orderRepository.findAllByString(new OrderSearch());
+        return all;
+    }
+}
