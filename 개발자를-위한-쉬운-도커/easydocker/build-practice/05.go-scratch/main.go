ec4e262301e9b2468438c41b56dde49f2d6e83ac
7unho <rlawnsgh8395@naver.com>
2024-06-02 10:35:44 +0900

    :bug: Fix: 파일 수정

--- 개발자를-위한-쉬운-도커/easydocker/build-practice/05.go-scratch/main.go
+++ 개발자를-위한-쉬운-도커/easydocker/build-practice/05.go-scratch/main.go
@@ -1,15 +1,15 @@
 package main
 
-import {
-	"fmt",
-	"net/http"
-}
+import (
+    "fmt"
+    "net/http"
+)
 
 func main() {
-	http.handler(",", func(w http.responseWriter, r *http.Request) {
-		fmt.Fprintf(w, "GO Scratch World!")
-	})
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        fmt.Fprintf(w, "Go, Scratch World!")
+    })
 
-	fmt.Println("Server is running on Port 8080")
-	http.ListenAndServe(":8080", nil)
+    fmt.Println("Server is running on port 8080")
+    http.ListenAndServe(":8080", nil)
 }
