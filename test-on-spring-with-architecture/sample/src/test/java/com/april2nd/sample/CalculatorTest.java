13b039e2ce521253e882e1a45dc4e1dd3e0c9f61
7unho <rlawnsgh8395@naver.com>
2025-01-17 14:22:18 +0900

    :sparkles: Feat: 사칙연산 테스트 작성

--- test-on-spring-with-architecture/sample/src/test/java/com/april2nd/sample/CalculatorTest.java
+++ test-on-spring-with-architecture/sample/src/test/java/com/april2nd/sample/CalculatorTest.java
@@ -21,4 +21,52 @@ public class CalculatorTest {
         Assert.assertEquals(5, result);
 //        assertThat(result).isEqualTo(6);
     }
+
+    @Test
+    public void 곱셈_연산을_할_수_있다() throws Exception {
+        //given
+        long num1 = 2;
+        String operator = "*";
+        long num2 = 3;
+        Calculator calculator = new Calculator();
+
+        //when
+        long result = calculator.calculate(num1, operator, num2);
+
+        //then
+        Assert.assertEquals(6, result);
+//        assertThat(result).isEqualTo(6);
+    }
+
+    @Test
+    public void 뺼셈_연산을_할_수_있다() throws Exception {
+        //given
+        long num1 = 2;
+        String operator = "-";
+        long num2 = 3;
+        Calculator calculator = new Calculator();
+
+        //when
+        long result = calculator.calculate(num1, operator, num2);
+
+        //then
+        Assert.assertEquals(-1, result);
+//        assertThat(result).isEqualTo(6);
+    }
+
+    @Test
+    public void 나눗셈_연산을_할_수_있다() throws Exception {
+        //given
+        long num1 = 6;
+        String operator = "/";
+        long num2 = 3;
+        Calculator calculator = new Calculator();
+
+        //when
+        long result = calculator.calculate(num1, operator, num2);
+
+        //then
+        Assert.assertEquals(2, result);
+//        assertThat(result).isEqualTo(6);
+    }
 }
