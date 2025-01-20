4f95c267342a25534f35b3ddd85f06aa0e4376f4
7unho <rlawnsgh8395@naver.com>
2025-01-20 15:23:48 +0900

    :test_tube: Test: 테스트 동시성 문제 해결
    전체 테스트 시 동시성 문제로 인한 테스트 케이스 실패. 따라서 .sql 파일 읽어오는 방식으로 변경

--- test-on-spring-with-architecture/demo/src/test/java/com/april2nd/demo/repository/UserRepositoryTest.java
+++ test-on-spring-with-architecture/demo/src/test/java/com/april2nd/demo/repository/UserRepositoryTest.java
@@ -7,47 +7,25 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.jdbc.Sql;
 
 import java.util.Optional;
 //import org.springframework.test.context.TestPropertySource;
 
 @DataJpaTest(showSql = true)
-//@TestPropertySource("classpath:test-application.properties")
+@TestPropertySource("classpath:test-application.properties")
+@Sql("classpath:/sql/user-repository-test-data.sql")
 class UserRepositoryTest {
     @Autowired
     private UserRepository userRepository;
-    @Test
-    @DisplayName("UserRepository 연결 테스트")
-    public void UserRepository_가_제대로_연결되었다() throws Exception {
-        //given
-        UserEntity userEntity = new UserEntity();
-        userEntity.setEmail("rlawnsgh8395@naver.com");
-        userEntity.setAddress("SEOUL");
-        userEntity.setNickname("april2nd");
-        userEntity.setStatus(UserStatus.ACTIVE);
-        userEntity.setCertificationCode("aaaaaa-aa-aa-aa-aaaaaa");
-
-        //when
-        UserEntity result = userRepository.save(userEntity);
-
-        //then
-        assertThat(result.getId()).isNotNull();
-    }
 
     @Test
     @DisplayName("ID와 유저상태로 유저 데이터를 찾아올 수 있다")
     public void findByIdAndStatus_로_유저_데이터를_찾아올_수_있다() throws Exception {
         //given
-        UserEntity userEntity = new UserEntity();
-        userEntity.setId(1L);
-        userEntity.setEmail("rlawnsgh8395@naver.com");
-        userEntity.setAddress("SEOUL");
-        userEntity.setNickname("april2nd");
-        userEntity.setStatus(UserStatus.ACTIVE);
-        userEntity.setCertificationCode("aaaaaa-aa-aa-aa-aaaaaa");
 
         //when
-        userRepository.save(userEntity);
         Optional<UserEntity> result = userRepository.findByIdAndStatus(1, UserStatus.ACTIVE);
 
         //then
@@ -58,16 +36,8 @@ class UserRepositoryTest {
     @DisplayName("ID와 유저상태로 조회 시 데이터가 없다면 optional.empty를 내려준다")
     public void findByIdAndStatus_는_데이터가_없으면_Optional_empty_를_내려준다() throws Exception {
         //given
-        UserEntity userEntity = new UserEntity();
-        userEntity.setId(1L);
-        userEntity.setEmail("rlawnsgh8395@naver.com");
-        userEntity.setAddress("SEOUL");
-        userEntity.setNickname("april2nd");
-        userEntity.setStatus(UserStatus.ACTIVE);
-        userEntity.setCertificationCode("aaaaaa-aa-aa-aa-aaaaaa");
 
         //when
-        userRepository.save(userEntity);
         Optional<UserEntity> result = userRepository.findByIdAndStatus(1, UserStatus.PENDING);
 
         //then
@@ -78,16 +48,8 @@ class UserRepositoryTest {
     @DisplayName("이메일과 유저상태로 유저 데이터를 찾아올 수 있다")
     public void findByEmailAndStatus_로_유저_데이터를_찾아올_수_있다() throws Exception {
         //given
-        UserEntity userEntity = new UserEntity();
-        userEntity.setId(1L);
-        userEntity.setEmail("rlawnsgh8395@naver.com");
-        userEntity.setAddress("SEOUL");
-        userEntity.setNickname("april2nd");
-        userEntity.setStatus(UserStatus.ACTIVE);
-        userEntity.setCertificationCode("aaaaaa-aa-aa-aa-aaaaaa");
 
         //when
-        userRepository.save(userEntity);
         Optional<UserEntity> result = userRepository.findByEmailAndStatus("rlawnsgh8395@naver.com", UserStatus.ACTIVE);
 
         //then
@@ -98,16 +60,8 @@ class UserRepositoryTest {
     @DisplayName("이메일과 유저상태로 조회 시 데이터가 없다면 optional.empty를 내려준다")
     public void findByEmailAndStatus_는_데이터가_없으면_Optional_empty_를_내려준다() throws Exception {
         //given
-        UserEntity userEntity = new UserEntity();
-        userEntity.setId(1L);
-        userEntity.setEmail("rlawnsgh8395@naver.com");
-        userEntity.setAddress("SEOUL");
-        userEntity.setNickname("april2nd");
-        userEntity.setStatus(UserStatus.ACTIVE);
-        userEntity.setCertificationCode("aaaaaa-aa-aa-aa-aaaaaa");
 
         //when
-        userRepository.save(userEntity);
         Optional<UserEntity> result = userRepository.findByEmailAndStatus("rlawnsgh8395@naver.com", UserStatus.PENDING);
 
         //then
