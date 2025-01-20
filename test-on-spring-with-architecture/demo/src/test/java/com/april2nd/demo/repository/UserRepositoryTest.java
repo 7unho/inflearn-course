b42f1e05ca8b537ef918b4a6209ae20693590839
7unho <rlawnsgh8395@naver.com>
2025-01-20 14:09:41 +0900

    :test_tube: Test: `findByIdAndStatus로 유저 데이터를 찾아올 수 있다` 케이스 추가 for UserRepositoryTest

--- test-on-spring-with-architecture/demo/src/test/java/com/april2nd/demo/repository/UserRepositoryTest.java
+++ test-on-spring-with-architecture/demo/src/test/java/com/april2nd/demo/repository/UserRepositoryTest.java
@@ -7,6 +7,8 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.Optional;
 //import org.springframework.test.context.TestPropertySource;
 
 @DataJpaTest(showSql = true)
@@ -30,5 +32,25 @@ class UserRepositoryTest {
 
         //then
         assertThat(result.getId()).isNotNull();
+    }
+
+    @Test
+    @DisplayName("ID와 유저상태로 유저 데이터를 찾아올 수 있다")
+    public void findByIdAndStatus_로_유저_데이터를_찾아올_수_있다() throws Exception {
+        //given
+        UserEntity userEntity = new UserEntity();
+        userEntity.setId(1L);
+        userEntity.setEmail("rlawnsgh8395@naver.com");
+        userEntity.setAddress("SEOUL");
+        userEntity.setNickname("april2nd");
+        userEntity.setStatus(UserStatus.ACTIVE);
+        userEntity.setCertificationCode("aaaaaa-aa-aa-aa-aaaaaa");
+
+        //when
+        userRepository.save(userEntity);
+        Optional<UserEntity> result = userRepository.findByIdAndStatus(1, UserStatus.ACTIVE);
+
+        //then
+        assertThat(result.isPresent()).isTrue();
     }
 }
