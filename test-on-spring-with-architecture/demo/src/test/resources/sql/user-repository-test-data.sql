c2ef51e25efc289776086016f849513b8823ad01
7unho <rlawnsgh8395@naver.com>
2025-01-20 15:25:42 +0900

    :test_tube: Test: SQL 스크립트 추가 for UserReposoryTest test data

---
+++ test-on-spring-with-architecture/demo/src/test/resources/sql/user-repository-test-data.sql
@@ -0,0 +1,2 @@
+insert into `users` (`id`, `email`, `nickname`, `address`, `certification_code`, `status`, `last_login_at`)
+values (1, 'rlawnsgh8395@naver.com', 'april2nd', 'seoul', 'aaaaa-aaaaaaaaaa-aaaaa-aaaaa', 'ACTIVE', 0);
