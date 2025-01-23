7bdc954244f47995e09f0af3a4a9142d54d7c841
7unho <rlawnsgh8395@naver.com>
2025-01-23 14:00:01 +0900

    :wrench: Test: SQL 스크립트 생성
    테스트 이후 데이터 삭제

--- test-on-spring-with-architecture/demo/src/test/resources/sql/user-repository-test-data.sql
+++ test-on-spring-with-architecture/demo/src/test/resources/sql/user-repository-test-data.sql
@@ -1,2 +1,5 @@
 insert into `users` (`id`, `email`, `nickname`, `address`, `certification_code`, `status`, `last_login_at`)
 values (1, 'rlawnsgh8395@naver.com', 'april2nd', 'seoul', 'aaaaa-aaaaaaaaaa-aaaaa-aaaaa', 'ACTIVE', 0);
+
+insert into `users` (`id`, `email`, `nickname`, `address`, `certification_code`, `status`, `last_login_at`)
+values (2, 'user@pending.com', 'april2nd_pending', 'seoul', 'aaaaa-aaaaaaaaaa-aaaaa-aaaaa', 'PENDING', 0);
