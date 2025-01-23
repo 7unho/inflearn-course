a522191d04a865bc520f65960eb993e12b3f1b02
7unho <rlawnsgh8395@naver.com>
2025-01-23 17:35:11 +0900

    :white_check_mark: Test: postService create, update 테스트 추가
    강의 보기 전 작성

--- test-on-spring-with-architecture/demo/src/test/java/com/april2nd/demo/service/PostServiceTest.java
+++ test-on-spring-with-architecture/demo/src/test/java/com/april2nd/demo/service/PostServiceTest.java
@@ -1,6 +1,8 @@
 package com.april2nd.demo.service;
 
 import com.april2nd.demo.exception.ResourceNotFoundException;
+import com.april2nd.demo.model.dto.PostCreateDto;
+import com.april2nd.demo.model.dto.PostUpdateDto;
 import com.april2nd.demo.repository.PostEntity;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -33,7 +35,7 @@ class PostServiceTest {
     @DisplayName("postId로 포스트를 불러올 수 있다")
     public void postId로_포스트를_불러올_수_있다() throws Exception {
         //given
-        Long postId = 1L;
+        Long postId = 100L;
 
         //when
         PostEntity result = postService.getPostById(postId);
@@ -50,5 +52,41 @@ class PostServiceTest {
         assertThatThrownBy(() -> {
             postService.getPostById(111L);
         }).isInstanceOf(ResourceNotFoundException.class);
+    }
+
+    @Test
+    @DisplayName("PostCrateDto로 새로운 포스트를 생성할 수 있다")
+    public void PostCrateDto로_새로운_포스트를_생성할_수_있다() throws Exception {
+        //given
+        PostCreateDto postCreateDto = PostCreateDto.builder()
+                .content("createdByPostCreateDto")
+                .writerId(100L)
+                .build();
+        //when
+        PostEntity result = postService.create(postCreateDto);
+
+        //then
+        assertThat(result.getWriter().getId()).isEqualTo(100L);
+
+        // TODO: 테스트 가능한 설계로 변경하기 ( PostServiceTest.create )
+        assertThat(result.getCreatedAt()).isGreaterThan(0L);
+        //assertThat(result.getCreatedAt()).isEqualTo(0L);
+    }
+
+    @Test
+    @DisplayName("PostUpdateDto로 포스트를 수정할 수 있다")
+    public void PostUpdateDto로_포스트를_수정할_수_있다() throws Exception {
+        //given
+        PostUpdateDto postUpdateDto = PostUpdateDto.builder()
+                .content("UpdatedPostByPostUpdateDto")
+                .build();
+        //when
+        PostEntity result = postService.update(100L, postUpdateDto);
+
+        //then
+        assertThat(result.getContent()).isEqualTo(postUpdateDto.getContent());
+        // TODO: 테스트 가능한 설계로 변경하기 ( PostServiceTest.update )
+        assertThat(result.getModifiedAt()).isGreaterThan(0L);
+        //assertThat(result.getModifiedAt()).isGreaterThan(0L);
     }
 }
--- test-on-spring-with-architecture/demo/src/test/resources/sql/post-service-test-data.sql
+++ test-on-spring-with-architecture/demo/src/test/resources/sql/post-service-test-data.sql
@@ -1,6 +1,8 @@
 insert into `users` (`id`, `email`, `nickname`, `address`, `certification_code`, `status`, `last_login_at`)
-values (1, 'kok202@naver.com', 'kok202', 'Seoul', 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa', 'ACTIVE', 0);
+values (100, 'rlawnsgh8395@naver.com', 'april2nd', 'seoul', 'aaaaa-aaaaaaaaaa-aaaaa-aaaaa', 'ACTIVE', 0);
+
 insert into `users` (`id`, `email`, `nickname`, `address`, `certification_code`, `status`, `last_login_at`)
-values (2, 'kok303@naver.com', 'kok303', 'Seoul', 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaab', 'PENDING', 0);
+values (200, 'user@pending.com', 'april2nd_pending', 'seoul', 'aaaaa-aaaaaaaaaa-aaaaa-aaaaa', 'PENDING', 0);
+
 insert into `posts` (`id`, `content`, `created_at`, `modified_at`, `user_id`)
-values (1, 'helloworld', 1678530673958, 0, 1);
+values (100, 'helloworld', 1678530673958, 0, 100);
