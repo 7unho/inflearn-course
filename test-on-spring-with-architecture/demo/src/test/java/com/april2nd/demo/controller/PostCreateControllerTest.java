335304a4510c01c08f7d44338148c25d8bde8fac
7unho <rlawnsgh8395@naver.com>
2025-02-15 14:37:02 +0900

    :white_check_mark: Test: PostCreateController 테스트 코드 추가

--- test-on-spring-with-architecture/demo/src/test/java/com/april2nd/demo/controller/PostCreateControllerTest.java
+++ test-on-spring-with-architecture/demo/src/test/java/com/april2nd/demo/controller/PostCreateControllerTest.java
@@ -1,4 +1,65 @@
+package com.april2nd.demo.controller;
+
+import com.april2nd.demo.model.dto.PostCreateDto;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlGroup;
+import org.springframework.test.web.servlet.MockMvc;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@AutoConfigureTestDatabase
+@SqlGroup({
+        @Sql(
+                value = "/sql/post-service-test-data.sql",
+                executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD
+        ),
+        @Sql(
+                value = "/sql/delete-all-data.sql",
+                executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD
+        )
+})
 class PostCreateControllerTest {
-  
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    @DisplayName("사용자는 게시물을 작성할 수 있다")
+    public void 사용자는_게시물을_작성할_수_있다() throws Exception {
+        // given
+        Long writerId = 100L;
+        String content = "CREATED post";
+        PostCreateDto postCreateDto = PostCreateDto.builder()
+                .writerId(writerId)
+                .content(content)
+                .build();
+
+        String requestBody = objectMapper.writeValueAsString(postCreateDto);
+        // when
+        // then
+        mockMvc.perform(
+                        post("/api/posts")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(requestBody)
+                ).andExpect(status().isCreated())
+                .andExpect(jsonPath("$.id").isNumber())
+                .andExpect(jsonPath("$.content").value(content))
+                .andExpect(jsonPath("$.createdAt").exists())
+                .andExpect(jsonPath("$.modifiedAt").doesNotExist())
+                .andExpect(jsonPath("$.writer.id").value(writerId));
+    }
 }
