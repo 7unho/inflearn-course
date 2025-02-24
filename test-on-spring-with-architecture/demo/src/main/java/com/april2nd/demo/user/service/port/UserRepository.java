9fea919f45338e8dfe86ff245a8670c4f37ca43d
7unho <rlawnsgh8395@naver.com>
2025-02-24 17:24:02 +0900

    :white_check_mark: Test: UserRepository 테스트 코드 작성 with 소형 테스트

--- test-on-spring-with-architecture/demo/src/main/java/com/april2nd/demo/user/service/port/UserRepository.java
+++ test-on-spring-with-architecture/demo/src/main/java/com/april2nd/demo/user/service/port/UserRepository.java
@@ -6,11 +6,11 @@ import com.april2nd.demo.user.domain.UserStatus;
 import java.util.Optional;
 
 public interface UserRepository {
+    Optional<User> findById(long id);
+
     Optional<User> findByIdAndStatus(long id, UserStatus userStatus);
 
     Optional<User> findByEmailAndStatus(String email, UserStatus userStatus);
 
     User save(User user);
-
-    Optional<User> findById(long id);
 }
