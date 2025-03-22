d7d4c6ab94a7b9d56aede5ee9c30af7859b5d242
7unho <rlawnsgh8395@naver.com>
2025-03-22 17:37:14 +0900

    :test_tube: Test: 여러명이 동시에 요청하는 경우 테스트 코드 작성

--- coupon-system/api/src/test/java/com/april2nd/api/service/ApplyServiceTest.java
+++ coupon-system/api/src/test/java/com/april2nd/api/service/ApplyServiceTest.java
@@ -5,6 +5,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
@@ -23,5 +27,33 @@ class ApplyServiceTest {
         Long count = couponRepository.count();
         // then
         assertThat(count).isEqualTo(1L);
+    }
+
+    @Test
+    public void 동시에_여러명_응모() throws Exception {
+        // given
+        int threadCount = 1000;
+
+        // multi-thread 환경을 위한 변수
+        ExecutorService executorService = Executors.newFixedThreadPool(32);
+        CountDownLatch latch = new CountDownLatch(threadCount);
+
+        // when
+        for (int i = 0; i < threadCount; i++) {
+            long userId = i;
+            executorService.submit(() -> {
+                try {
+                    applyService.apply(userId);
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+
+        latch.await();
+        long count = couponRepository.count();
+
+        // then
+        assertThat(count).isEqualTo(100L);
     }
 }
