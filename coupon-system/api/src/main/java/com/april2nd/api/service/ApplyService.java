30e1058a7ee6596586f9038d35ad25afefb245de
7unho <rlawnsgh8395@naver.com>
2025-03-23 10:58:30 +0900

    :sparkles: Feat: Set을 사용해 쿠폰 발급 개수 1개로 제한

--- coupon-system/api/src/main/java/com/april2nd/api/service/ApplyService.java
+++ coupon-system/api/src/main/java/com/april2nd/api/service/ApplyService.java
@@ -2,6 +2,7 @@ package com.april2nd.api.service;
 
 import com.april2nd.api.domain.Coupon;
 import com.april2nd.api.producer.CouponCreateProducer;
+import com.april2nd.api.repository.AppliedUserRepository;
 import com.april2nd.api.repository.CouponCountRepository;
 import com.april2nd.api.repository.CouponRepository;
 import org.springframework.stereotype.Service;
@@ -11,19 +12,25 @@ public class ApplyService {
     private final CouponRepository couponRepository;
     private final CouponCountRepository couponCountRepository;
     private final CouponCreateProducer couponCreateProducer;
+    private final AppliedUserRepository appliedUserRepository;
 
-    public ApplyService(CouponRepository couponRepository, CouponCountRepository couponCountRepository, CouponCreateProducer couponCreateProducer) {
+    public ApplyService(
+            CouponRepository couponRepository,
+            CouponCountRepository couponCountRepository,
+            CouponCreateProducer couponCreateProducer,
+            AppliedUserRepository appliedUserRepository) {
         this.couponRepository = couponRepository;
         this.couponCountRepository = couponCountRepository;
         this.couponCreateProducer = couponCreateProducer;
+        this.appliedUserRepository = appliedUserRepository;
     }
 
     public void apply(Long userId) {
-        long count = couponCountRepository.increment();
+        Long apply = appliedUserRepository.add(userId);
+        if (apply != 1) return; // 이미 발급했다면
 
-        if (count > 100) {
-            return;
-        }
+        long count = couponCountRepository.increment();
+        if (count > 100) return; // 수량이 초과됐다면
 
         couponCreateProducer.create(userId);
     }
