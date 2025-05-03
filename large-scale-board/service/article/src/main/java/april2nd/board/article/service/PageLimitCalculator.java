edeaa8773e12423b66cc335eb23280dbaccf3ccf
7unho <rlawnsgh8395@naver.com>
2025-05-03 11:58:21 +0900

    feat: 페이징 계산 utlityclass 작성

---
+++ large-scale-board/service/article/src/main/java/april2nd/board/article/service/PageLimitCalculator.java
@@ -0,0 +1,10 @@
+package april2nd.board.article.service;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class PageLimitCalculator {
+    public static Long calculatePageLimit(Long page, Long pageSize, Long movablePageCount) {
+        return ((page - 1) / movablePageCount + 1) * pageSize * movablePageCount + 1;
+    }
+}
