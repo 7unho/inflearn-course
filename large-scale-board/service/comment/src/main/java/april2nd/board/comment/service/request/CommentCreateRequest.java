f5181a4e45f520fdf4b1aa1ce61d9aba86926c5c
7unho <rlawnsgh8395@naver.com>
2025-05-04 17:06:52 +0900

    feat: comment 관련 entity, dto 객체 작성

---
+++ large-scale-board/service/comment/src/main/java/april2nd/board/comment/service/request/CommentCreateRequest.java
@@ -0,0 +1,11 @@
+package april2nd.board.comment.service.request;
+
+import lombok.Getter;
+
+@Getter
+public class CommentCreateRequest {
+    private Long articleId;
+    private String content;
+    private Long parentCommentId;
+    private Long writerId;
+}
---
+++ large-scale-board/service/comment/src/main/java/april2nd/board/comment/service/response/CommentResponse.java
@@ -0,0 +1,32 @@
+package april2nd.board.comment.service.response;
+
+import april2nd.board.comment.entity.Comment;
+import lombok.Getter;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+
+@Getter
+@ToString
+public class CommentResponse {
+    private Long commentId;
+    private String content;
+    private Long parentCommentId;
+    private Long articleId;
+    private Long writerId;
+    private Boolean deleted;
+    private LocalDateTime createdAt;
+
+    public static CommentResponse from(Comment comment) {
+        CommentResponse response = new CommentResponse();
+        response.commentId = comment.getCommentId();
+        response.parentCommentId = comment.getParentCommentId();
+        response.content = comment.getContent();
+        response.articleId = comment.getArticleId();
+        response.writerId = comment.getWriterId();
+        response.deleted = comment.getDeleted();
+        response.createdAt = comment.getCreatedAt();
+
+        return response;
+    }
+}
