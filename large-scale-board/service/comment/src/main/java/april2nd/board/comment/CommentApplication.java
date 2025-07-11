144426dc0caab5debfa750e8eb2d547117f7e66b
7unho <rlawnsgh8395@naver.com>
2025-07-11 14:40:23 +0900

    :heavy_plus_sign: Feat: MessageRelay 모듈 의존성 연

--- large-scale-board/service/comment/src/main/java/april2nd/board/comment/CommentApplication.java
+++ large-scale-board/service/comment/src/main/java/april2nd/board/comment/CommentApplication.java
@@ -2,8 +2,12 @@ package april2nd.board.comment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EntityScan(basePackages = "april2nd.board")
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "april2nd.board")
 public class CommentApplication {
     public static void main(String[] args) {
         SpringApplication.run(CommentApplication.class, args);
