0eef910bf46b71dba857f2481f3785dc4788ddd9
7unho <rlawnsgh8395@naver.com>
2025-07-11 15:03:39 +0900

    :sparkles: Feat: 조회 서비스 BackUp 로직에 이벤트 publish 로직 추가

--- large-scale-board/service/view/src/main/java/april2nd/board/view/service/ArticleViewCountBackUpProcessor.java
+++ large-scale-board/service/view/src/main/java/april2nd/board/view/service/ArticleViewCountBackUpProcessor.java
@@ -1,5 +1,8 @@
 package april2nd.board.view.service;
 
+import april2nd.board.common.event.EventType;
+import april2nd.board.common.event.payload.article.ArticleViewedEventPayload;
+import april2nd.board.common.outboxmessagerelay.OutboxEventPublisher;
 import april2nd.board.view.entity.ArticleViewCount;
 import april2nd.board.view.repository.ArticleViewCountBackUpRepository;
 import jakarta.transaction.Transactional;
@@ -9,6 +12,7 @@ import org.springframework.stereotype.Component;
 @Component
 @RequiredArgsConstructor
 public class ArticleViewCountBackUpProcessor {
+    private final OutboxEventPublisher outboxEventPublisher;
     private final ArticleViewCountBackUpRepository articleViewCountBackUpRepository;
 
     @Transactional
@@ -20,5 +24,14 @@ public class ArticleViewCountBackUpProcessor {
                             () -> articleViewCountBackUpRepository.save(ArticleViewCount.init(articleId, viewCount))
                     );
         }
+
+        outboxEventPublisher.publish(
+                EventType.ARTICLE_VIEWED,
+                ArticleViewedEventPayload.builder()
+                        .articleId(articleId)
+                        .articleViewCount(viewCount)
+                        .build(),
+                articleId
+        );
     }
 }
